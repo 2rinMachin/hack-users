6af7074d8f990394b18f10e9524b97f05791238a
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-16 21:19:18 -0500

    refactor: fix caching stuff

--- functions/verify_staff.py
+++ functions/verify_staff.py
@@ -10,12 +10,16 @@ session_tokens = dynamodb.Table("hack-user-tokens")
 
 
 def handler(event, context):
+    print("headers: ", event["headers"])
     authorization = str(event["headers"]["Authorization"])
     token = authorization.split(" ")[1]
 
     user = verify_session_token(token)
 
-    if user == None or user.role != UserRole.staff or user.role != UserRole.authority:
+    if user == None:
+        return auth.unauthorized(event)
+
+    if user.role != UserRole.staff and user.role != UserRole.authority:
         return auth.unauthorized(event)
 
     return auth.authorized(event, user)
